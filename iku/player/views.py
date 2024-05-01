49fcb17baf2876626c0ac42993129fd6faa24908
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-05-01 09:35:34 +0000

    Display and input new songs

--- iku/player/views.py
+++ iku/player/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    songs = Song.objects.all()
+    context = {
+        "songs":songs,
+    }
+    return render(request, "index.html", context)
