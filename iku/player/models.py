f31b61a1db45bfafd1377d3e9b81ee027739622c
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-05-01 08:50:45 +0000

    models

--- iku/player/models.py
+++ iku/player/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class song(models.Model):
-    pass
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class Song(models.Model):
+    author = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    song_file = models.FileField()
+    song_name = models.CharField(max_length=100)
+    song_description = models.CharField(max_length=1000)
+    song_lenght = models.DecimalField(decimal_places=2, max_digits=200)
