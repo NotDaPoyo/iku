435a5bcc4e3a7629a2d4e319c81916d3a52a180c
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-05-01 09:04:42 +0000

    First song and 3 users

--- iku/player/models.py
+++ iku/player/models.py
@@ -3,13 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    likes = models.DecimalField(decimal_places=0, max_digits=999999999)
+
+    def __str__(self):
+        return self.name
 
 
 class Song(models.Model):
     author = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    song_file = models.FileField()
-    song_name = models.CharField(max_length=100)
-    song_description = models.CharField(max_length=1000)
-    song_lenght = models.DecimalField(decimal_places=2, max_digits=200)
+    file = models.FileField(upload_to="songs")
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    lenght = models.DecimalField(decimal_places=2, max_digits=200)
+
+    def __str__(self):
+        return self.name
