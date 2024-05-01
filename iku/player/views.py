8fa7d4ba7f202951d856aa43c559e714ba5401dc
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-05-01 15:21:04 +0000

    Volume slider

--- iku/player/views.py
+++ iku/player/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from mutagen.mp3 import MP3
+
 
 # Create your views here.
 def home(request, id=1):
     songs = Song.objects.all()
     selected_song = Song.objects.get(pk=id)
     song_location = selected_song.file
+    song_lenght = MP3(selected_song.file)
     context = {
         "songs":songs,
         "selected_song":selected_song,
         "song_location":song_location,
+        "song_lenght":song_lenght.info.length
     }
     
     return render(request, "index.html", context)
