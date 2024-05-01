6533a118d8ccac844d7a73a9398f9d13d69cc5cf
NotDaPoyo <168030791+NotDaPoyo@users.noreply.github.com>
2024-05-01 10:25:39 +0000

    trying to play audio files

--- iku/player/views.py
+++ iku/player/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
-def home(request):
+def home(request, id=1):
     songs = Song.objects.all()
+    selected_song = Song.objects.get(pk=id)
+    song_location = selected_song.file
     context = {
         "songs":songs,
+        "selected_song":selected_song,
+        "song_location":song_location,
     }
+    
     return render(request, "index.html", context)
